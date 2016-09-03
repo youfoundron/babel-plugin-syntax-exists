4b82a49f2d97e44e8533bbf32328488faa7697ff
Ron Gierlach <rongierlach@gmail.com>
2016-09-03 16:18:41 -0500

    test for thrown error on transformation

--- test/index.js
+++ test/index.js
@@ -6,10 +6,20 @@ import syntax_exists from '../src'
 import { transformFileSync, transformFile } from 'babel-core'
 
 function testTransformation(actual, expected) {
-  const actual_val = eval(actual)
-  const expected_val = eval(expected)
-  // console.log("ACTUAL:", actual_val, actual)
-  // console.log("EXPECTED:", expected_val, expected)
+  let actual_val, expected_val
+
+  try {
+    actual_val = eval(actual)
+    expected_val = eval(expected)
+  } catch(err) {
+    console.log("    Cannot transform the src!")
+    expect(eval(actual)).to.not.throw(Error)
+  }
+
+  console.log(
+    `    (${actual_val} === ${expected_val}) // =>`,
+    actual_val === expected_val
+  )
   expect(actual_val).to.equal(expected_val)
 }
 
@@ -19,11 +29,11 @@ describe('BabelSyntaxExistsPluginTest', () => {
 
   fs.readdirSync(fixtures_dir).map((case_name) => {
     it(`should ${case_name.split('-').join(' ')}`, () => {
-      // Test Paths
+      // paths
       const fixture_dir = path.join(fixtures_dir, case_name)
       const actual_path = path.join(fixture_dir, 'actual.js')
       const expected_path = path.join(fixture_dir, 'expected.js')
-      // Test code
+      // scripts
       const actual = transformFileSync(actual_path, options).code
       const expected = fs.readFileSync(expected_path).toString()
       // Test it!
